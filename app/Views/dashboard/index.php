7b4d919f2178f48be0e6771b2ad293e5e397f245
Hutomo Sugianto <hutomosugianto@gmail.com>
2025-01-28 10:38:39 +0700

    Update View for Dashboard Index
    Update View for Dashboard Index

--- app/Views/dashboard/index.php
+++ app/Views/dashboard/index.php
@@ -1,1 +1,5 @@
 <?= $this->extend('layouts/adminlte_3') ?>
+
+<?= $this->section('page_title') ?>Dashboard<?= $this->endSection() ?>
+
+<?= $this->section('header_content') ?><?= $this->endSection() ?>
