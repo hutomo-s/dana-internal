b02560a15ad32ced803ccce808a11366996308ed
Hutomo Sugianto <hutomosugianto@gmail.com>
2025-01-29 07:21:57 +0700

    Formatting ep_create.js using 2 spaces
    Formatting ep_create.js using 2 spaces

--- public/assets/js/ep_create.js
+++ public/assets/js/ep_create.js
@@ -1,76 +1,76 @@
 $(function () {
-    bsCustomFileInput.init();
+  bsCustomFileInput.init();
 });
 
 var today = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate());
 
 $("input[name=request_due_date]").datepicker({
-    uiLibrary: "bootstrap4",
-    format: "yyyy-mm-dd",
-    minDate: today
+  uiLibrary: "bootstrap4",
+  format: "yyyy-mm-dd",
+  minDate: today
 });
 
-$("#ep-create-form").on("submit", function(e) {
-    // stop form from submitting normally
-    e.preventDefault();
+$("#ep-create-form").on("submit", function (e) {
+  // stop form from submitting normally
+  e.preventDefault();
 
-    // reset modal body content
-    $("#modal-default-body").html("");
+  // reset modal body content
+  $("#modal-default-body").html("");
 
-    var $form = $(this);
+  var $form = $(this);
 
-    var url = $form.attr("action");
+  var url = $form.attr("action");
 
-    var form_data = new FormData(this); 
+  var form_data = new FormData(this);
 
-    // send the data using post
-    $.ajax({
-        url: url,
-        type: "POST",
-        data: form_data,
-        contentType: false,
-        cache: false,
-        processData: false,
-        success: function(result) {
-            
-            if(result.redirect_url) {
-                alert(result.message);
-                window.location = result.redirect_url;
-            } 
-            else {
-                var container = $("<div />");
-                var message = `<p>${result.message}</p>`;
-                container.append(message);
-                $("#modal-default-body").html(container);
-                $("#modal-default").modal("show");
-            }
-        },
-        error: function(response) {
+  // send the data using post
+  $.ajax({
+    url: url,
+    type: "POST",
+    data: form_data,
+    contentType: false,
+    cache: false,
+    processData: false,
+    success: function (result) {
 
-            var result = response.responseJSON;
+      if (result.redirect_url) {
+        alert(result.message);
+        window.location = result.redirect_url;
+      }
+      else {
+        var container = $("<div />");
+        var message = `<p>${result.message}</p>`;
+        container.append(message);
+        $("#modal-default-body").html(container);
+        $("#modal-default").modal("show");
+      }
+    },
+    error: function (response) {
 
-            var container = $("<div />");
+      var result = response.responseJSON;
 
-            var message = `<p>${result.message}</p>`;
+      var container = $("<div />");
 
-            container.append(message);
+      var message = `<p>${result.message}</p>`;
 
-            if(result.error_messages) {
+      container.append(message);
 
-                var ul = $("<ul />");
+      if (result.error_messages) {
 
-                result.error_messages.forEach(em => {
-                    var em = `<li>${em}</li>`;
-                    ul.append(em);
-                });
+        var ul = $("<ul />");
 
-                container.append(ul);
-            }
+        result.error_messages.forEach(em => {
+          var em = `<li>${em}</li>`;
+          ul.append(em);
+        });
 
-            $("#modal-default-body").html(container);
-            
-            $("#modal-default").modal("show");
+        container.append(ul);
+      }
 
-        }
-    });
+      $("#modal-default-body").html(container);
+
+      $("#modal-default").modal("show");
+
+    }
+  });
 })
